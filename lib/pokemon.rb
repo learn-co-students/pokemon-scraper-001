1924901f04a6bee4a52c03efb485f135c2e67787
halfields <hfields697@aol.com>
2016-05-17 09:50:28 -0500

    done

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,51 @@
+require 'pry'
+
 class Pokemon
+	attr_accessor :id, :name, :type, :db
+
+	def initialize(id, name, type, db)
+		@id = id
+		@name = name
+		@type = type
+		@db = db
+	end
+
+	def self.find(id, db)
+	  sql = <<-EOM
+      SELECT *
+      FROM pokemon
+      WHERE id = ?
+      LIMIT 1
+      EOM
+
+      db.execute(sql, id).flatten 
+	end
+
+	def self.insert(name, type, db)
+		sql = <<-EOM
+		INSERT INTO pokemon
+		(name, type)
+		VALUES
+		(?, ?)
+		EOM
+
+		db.execute(sql, name, type)
+	end
+
+	def update(name, type, db)
+		sql = <<-EOM
+		UPDATE pokemon
+		SET (name = ?, type = ?)
+		WHERE id = ?
+		EOM
+
+		db.execute(sql, name, type)
+	end
+
+  def self.save(name, type, db)
+  	 insert(name, type, db)
+   # persisted? ? update(name, type, db) : insert(name, type, db)
+  #  binding.pry
+
+  end
 end
